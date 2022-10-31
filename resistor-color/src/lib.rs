cdc16fb1c416cb0ddb3211d55cf991ec197f69c6
weykon <635883092@qq.com>
2022-10-31 12:27:22 +0800

    save

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -13,7 +13,7 @@ pub enum ResistorColor {
 }
 
 pub fn color_to_value(_color: ResistorColor) -> u32 {
-    unimplemented!("convert a color into a numerical representation")
+    ResistorColor[_color]
 }
 
 pub fn value_to_color_string(value: u32) -> String {
