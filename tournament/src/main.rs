43b0795746ed86c8c5e5c4f0679300d41c623d48
weykon <635883092@qq.com>
2022-12-21 14:35:15 +0800

    tournament

---
+++ tournament/src/main.rs
@@ -0,0 +1,14 @@
+use tournament::tally;
+
+fn main() {
+    let result = tally(
+        "Allegoric Alaskans;Blithering Badgers;win
+Devastating Donkeys;Courageous Californians;draw
+Devastating Donkeys;Allegoric Alaskans;win
+Courageous Californians;Blithering Badgers;loss
+Blithering Badgers;Devastating Donkeys;loss
+Allegoric Alaskans;Courageous Californians;win",
+    );
+
+    println!("{:?}",result);
+}
