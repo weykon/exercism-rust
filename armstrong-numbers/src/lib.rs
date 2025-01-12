b7dee1a431cf8f84ecfc6780b7bc3ca15296bc56
weykon <635883092@qq.com>
2025-01-12 20:08:28 +0800

    armstrong number

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,21 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    // unimplemented!("none");
+    let bit_count = num.to_string().len();
+    num.to_string().as_str().chars().fold(0, |now, num_char| {
+        let to_num = num_char.to_digit(10).unwrap();
+        now + to_num.pow(bit_count as u32)
+    }) == num
+}
+
+#[test]
+fn my_test() {
+    // check how to_digit work
+    let num_1: u32 = 11;
+    let num_2: u32 = 22;
+    num_1.to_string().as_str().chars().for_each(|num_c| {
+        println!("1 {:?}", num_c.to_digit(10).unwrap());
+    });
+    num_2.to_string().as_str().chars().for_each(|num_c| {
+        println!("2 {:?}", num_c.to_digit(10).unwrap());
+    });
 }
